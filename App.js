0b4866ef6aff46f061bf406375f678548336b5d7
Austinspeed <austinikedinma@gmail.com>
2026-02-23 08:13:09 +0100

    Added logic for playing game and game over screen

--- App.js
+++ App.js
@@ -1,33 +1,53 @@
 import { useState } from "react";
 
 import { StyleSheet, ImageBackground } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { LinearGradient } from "expo-linear-gradient";
 
 import StartGameScreen from "./screen/StartGameScreen";
 import GameScreen from "./screen/GameScreen";
+import Colors from "./constants/colors";
+import GameOverScreen from "./screen/GameOverScreen";
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
+  const [gameIsOver, setGameIsOver] = useState(true);
+
+  const gameOverHandler = () => {
+    setGameIsOver(true);
+  };
 
   const pickedNumberHandler = (pickedNumber) => {
     setUserNumber(pickedNumber);
+    setGameIsOver(false)
   };
 
-  let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />
+  let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
 
   if (userNumber) {
-    screen = <GameScreen />
+    screen = (
+      <GameScreen userNumber={userNumber} onGameOver={gameOverHandler} />
+    );
+  }
+
+  if (gameIsOver && userNumber) {
+    screen = <GameOverScreen />
   }
 
   return (
-    <LinearGradient colors={["#72063c", "#ddb52f"]} style={styles.rootScreen}>
+    <LinearGradient
+      colors={[Colors.primary500, Colors.accent500]}
+      style={styles.rootScreen}
+    >
       <ImageBackground
         source={require("./assets/images/background.png")}
         style={styles.rootScreen}
         resizeMode="cover"
         imageStyle={styles.backgroundImage}
       >
-        {screen}
+        <SafeAreaProvider>
+          <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
+        </SafeAreaProvider>
       </ImageBackground>
     </LinearGradient>
   );
--- screen/GameScreen.js
+++ screen/GameScreen.js
@@ -1,15 +1,86 @@
-import { View, Text } from "react-native";
+import { useEffect, useState, useRef } from "react";
 
-const GameScreen = () => {
-  return <View>
-    <Text>Opponent's Guess</Text>
-    GUESS
-    <View>
+import { View, Text, StyleSheet, Alert } from "react-native";
+import Title from "../component/ui/Title";
+import NumberContainer from "../component/game/NumberContainer";
+import PrimaryButton from "../component/ui/PrimaryButton";
+
+function generateRandomBetween(min, max, exclude) {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+}
+
+const GameScreen = ({userNumber, onGameOver}) => {
+  const minBoundary = useRef(1)
+  const maxBoundary = useRef(100)
+
+  const initialGuess = generateRandomBetween(
+    minBoundary.current,
+    maxBoundary.current,
+    userNumber,
+  );
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+  useEffect(() => {
+    if (currentGuess === userNumber) {
+      onGameOver();
+    }
+  }, [currentGuess, userNumber, onGameOver])
+
+  const nextGuessHandler = (direction) => {
+    if (
+      (direction === "lower" && currentGuess < userNumber) ||
+      (direction === "greater" && currentGuess > userNumber)
+    ) {
+      Alert.alert("Don't lie!", "You know that this is wrong...", [
+        { text: "Sorry!", style: "cancel" },
+      ]);
+      return;
+    }
+
+    if (direction === "lower") {
+      maxBoundary.current = currentGuess;
+    } else {
+      minBoundary.current = currentGuess + 1;
+    }
+    const newRndNumber = generateRandomBetween(
+      minBoundary.current,
+      maxBoundary.current,
+      currentGuess,
+    );
+    setCurrentGuess(newRndNumber);
+  };
+
+  return (
+    <View style={styles.screen}>
+      <Title>Opponent's Guess</Title>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <View>
         <Text>Higher or lower?</Text>
-        + -
+        <View>
+          <PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>
+            -
+          </PrimaryButton>
+          <PrimaryButton onPress={nextGuessHandler.bind(this, "greater")}>
+            +
+          </PrimaryButton>
+        </View>
+      </View>
+      {/* <View>LOG ROUNDS</View> */}
     </View>
-    <View>LOG ROUNDS</View>
-  </View>;
+  );
 };
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+});
